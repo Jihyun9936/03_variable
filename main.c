e5ab27ce615bd813e4ff783c1998f6248fd5e1d0
Jihyun9936 <55350908+Jihyun9936@users.noreply.github.com>
2019-09-19 15:32:00 +0900

    Lab02

--- main.c
+++ main.c
@@ -3,9 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(void) {
+	int x;
+	char y;
+	short z;
+	float a;
+	double b;
 	
-	printf("Hello World!!!\n");
-	
-	return 0;
+	printf("bytes of int : %d\n", sizeof(x));
+	printf("bytes of char : %d\n", sizeof(y));
+	printf("bytes of short : %d\n", sizeof(z));
+	printf("bytes of float : %d\n", sizeof(a));
+	printf("bytes of double : %d\n", sizeof(b));
 }
